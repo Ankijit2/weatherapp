2527b18accbe3ff3dfe1da3abf796860c305ad0d
ANKIJIT  ROY <149407832+Ankijit2@users.noreply.github.com>
2023-12-10 08:55:21 +0530

    Update weatherapi.js

--- src/hooks/weatherapi.js
+++ src/hooks/weatherapi.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
-
+const api = import.meta.env.VITE_APIKEY;
 function useWeather(locations) {
     
     const [data, setData] = useState(null);
 
     useEffect(() => {
         
-        fetch(`https://api.weatherapi.com/v1/current.json?key=2fb0063579934aefb8451233231511&q=${locations}&aqi=no`)
+        fetch(`http://api.weatherapi.com/v1/current.json?key=${api}&q=${locations}&aqi=no`)
             .then((res) => res.json())
             .then((res) => {
                 setData(res);
